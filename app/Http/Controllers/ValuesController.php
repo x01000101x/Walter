6a2d8260db6ed98b6b603861e34e45212cb72a04
x01000101x <leodamanik619@gmail.com>
2021-08-17 18:24:02 +0700

    couple of things

--- app/Http/Controllers/ValuesController.php
+++ app/Http/Controllers/ValuesController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Facade\FlareClient\View;
 use Illuminate\Http\Request;
 
 class ValuesController extends Controller
 {
-    //
+    public function show()
+    {
+        return View("welcome");
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ValuesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [ValuesController::class, 'show']);
