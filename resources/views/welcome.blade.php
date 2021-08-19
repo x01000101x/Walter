6b261dfa9eb0cfb24376da3de6cd7da0f7db26d6
x01000101x <leodamanik619@gmail.com>
2021-08-19 17:32:05 +0700

    added some views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -110,32 +110,18 @@
                             <div class="card-header ">
                                 <div class="row">
                                     <div class="col-sm-6 text-left">
-                                        <h5 class="card-category">Total Shipments</h5>
-                                        <h2 class="card-title">Performance</h2>
+                                        <h5 class="card-category">Real-time</h5>
+                                        <h2 class="card-title">Soil Moisture Graphic</h2>
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="btn-group btn-group-toggle float-right" data-toggle="buttons">
                                             <label class="btn btn-sm btn-primary btn-simple active" id="0">
                                                 <input type="radio" name="options" checked>
-                                                <span class="d-none d-sm-block d-md-block d-lg-block d-xl-block">Accounts</span>
+                                                <span class="d-none d-sm-block d-md-block d-lg-block d-xl-block">Linear graphic</span>
                                                 <span class="d-block d-sm-none">
                                                     <i class="tim-icons icon-single-02"></i>
                                                 </span>
-                                            </label>
-                                            <label class="btn btn-sm btn-primary btn-simple" id="1">
-                                                <input type="radio" class="d-none d-sm-none" name="options">
-                                                <span class="d-none d-sm-block d-md-block d-lg-block d-xl-block">Purchases</span>
-                                                <span class="d-block d-sm-none">
-                                                    <i class="tim-icons icon-gift-2"></i>
-                                                </span>
-                                            </label>
-                                            <label class="btn btn-sm btn-primary btn-simple" id="2">
-                                                <input type="radio" class="d-none" name="options">
-                                                <span class="d-none d-sm-block d-md-block d-lg-block d-xl-block">Sessions</span>
-                                                <span class="d-block d-sm-none">
-                                                    <i class="tim-icons icon-tap-02"></i>
-                                                </span>
-                                            </label>
+
                                         </div>
                                     </div>
                                 </div>
