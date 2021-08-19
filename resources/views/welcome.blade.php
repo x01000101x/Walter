181d1e5322d55bde25ac0cd66f170c3a333d491c
x01000101x <leodamanik619@gmail.com>
2021-08-19 18:45:35 +0700

    button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -146,8 +146,14 @@
                             </div>
                             <div class="card-body">
                                 <div class="chart-area">
-                                    <canvas id="CountryChart"></canvas>
-                                </div>
+                                    <div class="vertical-center">
+
+                                            <button style="" class="button button5">Siram</button>
+
+                                        </div>
+                                            <canvas id="CountryChart">
+                                            </canvas>
+                                        </div>
                             </div>
                         </div>
                     </div>
@@ -166,162 +172,12 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-lg-6 col-md-12">
-                        <div class="card card-tasks">
-                            <div class="card-header ">
-                                <h6 class="title d-inline">Tasks(5)</h6>
-                                <p class="card-category d-inline">today</p>
-                                <div class="dropdown">
-                                    <button type="button" class="btn btn-link dropdown-toggle btn-icon" data-toggle="dropdown">
-                                        <i class="tim-icons icon-settings-gear-63"></i>
-                                    </button>
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
-                                        <a class="dropdown-item" href="#pablo">Action</a>
-                                        <a class="dropdown-item" href="#pablo">Another action</a>
-                                        <a class="dropdown-item" href="#pablo">Something else</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="card-body ">
-                                <div class="table-full-width table-responsive">
-                                    <table class="table">
-                                        <tbody>
-                                            <tr>
-                                                <td>
-                                                    <div class="form-check">
-                                                        <label class="form-check-label">
-                                                            <input class="form-check-input" type="checkbox" value="">
-                                                            <span class="form-check-sign">
-                                                                <span class="check"></span>
-                                                            </span>
-                                                        </label>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <p class="title">Update the Documentation</p>
-                                                    <p class="text-muted">Dwuamish Head, Seattle, WA 8:47 AM</p>
-                                                </td>
-                                                <td class="td-actions text-right">
-                                                    <button type="button" rel="tooltip" title="" class="btn btn-link" data-original-title="Edit Task">
-                                                        <i class="tim-icons icon-pencil"></i>
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <div class="form-check">
-                                                        <label class="form-check-label">
-                                                            <input class="form-check-input" type="checkbox" value="" checked="">
-                                                            <span class="form-check-sign">
-                                                                <span class="check"></span>
-                                                            </span>
-                                                        </label>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <p class="title">GDPR Compliance</p>
-                                                    <p class="text-muted">The GDPR is a regulation that requires businesses to protect the personal data and privacy of Europe citizens for transactions that occur within EU member states.</p>
-                                                </td>
-                                                <td class="td-actions text-right">
-                                                    <button type="button" rel="tooltip" title="" class="btn btn-link" data-original-title="Edit Task">
-                                                        <i class="tim-icons icon-pencil"></i>
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <div class="form-check">
-                                                        <label class="form-check-label">
-                                                            <input class="form-check-input" type="checkbox" value="">
-                                                            <span class="form-check-sign">
-                                                                <span class="check"></span>
-                                                            </span>
-                                                        </label>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <p class="title">Solve the issues</p>
-                                                    <p class="text-muted">Fifty percent of all respondents said they would be more likely to shop at a company </p>
-                                                </td>
-                                                <td class="td-actions text-right">
-                                                    <button type="button" rel="tooltip" title="" class="btn btn-link" data-original-title="Edit Task">
-                                                        <i class="tim-icons icon-pencil"></i>
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <div class="form-check">
-                                                        <label class="form-check-label">
-                                                            <input class="form-check-input" type="checkbox" value="">
-                                                            <span class="form-check-sign">
-                                                                <span class="check"></span>
-                                                            </span>
-                                                        </label>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <p class="title">Release v2.0.0</p>
-                                                    <p class="text-muted">Ra Ave SW, Seattle, WA 98116, SUA 11:19 AM</p>
-                                                </td>
-                                                <td class="td-actions text-right">
-                                                    <button type="button" rel="tooltip" title="" class="btn btn-link" data-original-title="Edit Task">
-                                                        <i class="tim-icons icon-pencil"></i>
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <div class="form-check">
-                                                        <label class="form-check-label">
-                                                            <input class="form-check-input" type="checkbox" value="">
-                                                            <span class="form-check-sign">
-                                                                <span class="check"></span>
-                                                            </span>
-                                                        </label>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <p class="title">Export the processed files</p>
-                                                    <p class="text-muted">The report also shows that consumers will not easily forgive a company once a breach exposing their personal data occurs. </p>
-                                                </td>
-                                                <td class="td-actions text-right">
-                                                    <button type="button" rel="tooltip" title="" class="btn btn-link" data-original-title="Edit Task">
-                                                        <i class="tim-icons icon-pencil"></i>
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <div class="form-check">
-                                                        <label class="form-check-label">
-                                                            <input class="form-check-input" type="checkbox" value="">
-                                                            <span class="form-check-sign">
-                                                                <span class="check"></span>
-                                                            </span>
-                                                        </label>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <p class="title">Arival at export process</p>
-                                                    <p class="text-muted">Capitol Hill, Seattle, WA 12:34 AM</p>
-                                                </td>
-                                                <td class="td-actions text-right">
-                                                    <button type="button" rel="tooltip" title="" class="btn btn-link" data-original-title="Edit Task">
-                                                        <i class="tim-icons icon-pencil"></i>
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
+
                     <div class="col-lg-6 col-md-12">
                         <div class="card ">
                             <div class="card-header">
-                                <h4 class="card-title"> Simple Table</h4>
+                                <h4 class="card-title"> Percentage Table</h4>
                             </div>
                             <div class="card-body">
                                 <div class="table-responsive">
@@ -339,6 +195,9 @@
                                                 </th>
                                                 <th class="text-center">
                                                     Time
+                                                </th>
+                                                <th class="text-center">
+                                                    Day
                                                 </th>
                                             </tr>
                                         </thead>
