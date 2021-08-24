5c03dc28304141a6f5b8b7e36e03ec1d67b41bc7
x01000101x <leodamanik619@gmail.com>
2021-08-24 15:46:48 +0700

    git

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,11 +13,11 @@
     <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
     <!-- Nucleo Icons -->
-    <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="{{ url('/') }}/assets/css/nucleo-icons.css" rel="stylesheet" />
     <!-- CSS Files -->
-    <link href="../assets/css/black-dashboard.css?v=1.0.0" rel="stylesheet" />
+    <link href="{{ url('/') }}/assets/css/black-dashboard.css?v=1.0.0" rel="stylesheet" />
     <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link href="../assets/demo/demo.css" rel="stylesheet" />
+    <link href="{{ url('/') }}/assets/demo/demo.css" rel="stylesheet" />
 </head>
 
 <body class="">
