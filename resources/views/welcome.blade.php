e2b99144a6022a4f0a2fe8306c199434bec1af1d
x01000101x <leodamanik619@gmail.com>
2021-09-09 11:05:40 +0700

    button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -177,8 +177,9 @@
                 <div class="col-lg-4">
                     <div class="card card-chart">
                         <div class="card-header">
-                            <h5 class="card-category">Completed Tasks</h5>
-                            <h3 class="card-title"><i class="tim-icons icon-send text-success"></i> 12,100K</h3>
+                            <h5 class="card-category">Real-time</h5>
+                            <h3 class="card-title"><i class="tim-icons icon-send text-success"></i>PUPUK graphic
+                            </h3>
                         </div>
                         <div class="card-body">
                             <div class="chart-area">
